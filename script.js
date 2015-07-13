487f9d2f61710195f81e30cdb6c0f6e723bcba4c
HectorRamosJunior <HectorRamos.Junior@gmail.com>
2015-07-12 21:45:04 -0400

    Trying to add Remove Button

--- script.js
+++ script.js
@@ -4,9 +4,26 @@
 $(document).ready(function() {
   var list = document.getElementById("list");
 
-  $("#button").on("click", function(){
-    var toDo = "<li>" + document.getElementById("textfield").value + "</li>";
+  $("#button").on("click", function() { 
+    var toDo = document.getElementById("textfield").value;
+    toDo = "<li><input type='checkbox'> " + toDo + " </li>"
+    $("#textfield").val("");
 
     $(list).append(toDo);
   });
+
+
+
+  $("#removeButton").on("click", function() {
+    $("#list").each(function() {
+      if( $(this).is(":checked") ) {
+      $(this).remove();
+    }
+    });
+
+
+
+
+  });
+
 });
