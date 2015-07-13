c519833239c0e17ea9729dc66a97bfb646609cc3
Steve Mostovoy <stevemostovoysm@gmail.com>
2015-07-13 00:35:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-﻿/* Hector Ramos */
+/* Hector Ramos */
 /* July 8, 2015 */
 
 $(document).ready(function() {
-  var list = document.getElementById("list");
-  $("#textfield").focus();
-
-  $("#textfield").on("keypress", function(e) {
-    if(e.which === 13) {
-      var toDo = document.getElementById("textfield").value;
-      toDo = "<li><input type='checkbox'> " + toDo + " </li>"
-      $("#textfield").val("");
-
-      $(list).append(toDo);
-      $("#textfield").focus();
+  var $list = $("#list");
+  var $textfield = $("#textfield");
+  var $button = $("#button");
+  var $removeButton = $("#removeButton");
+  
+  $textfield.focus();
+  $textfield.on("keypress", function(key) {
+    if (key.which === 13) {
+      appendItem($textfield);
     }
   });
 
-
-
-
-  $("#button").on("click", function() { 
-    var toDo = document.getElementById("textfield").value;
-    toDo = "<li><input type='checkbox'> " + toDo + " </li>"
-    $("#textfield").val("");
-
-    $(list).append(toDo);
-    $("#textfield").focus();
+  $button.on("click", function() {
+    appendItem($textfield);
   });
 
-
-
-  $("#removeButton").on("click", function() {
-
-    $($("#list").children()).each( function() {
-      if( ($(this).children("input").is(":checked")) )
+  $removeButton.on("click", function() {
+    $list.children().each(function() {
+      if (($(this).children("input").is(":checked"))) {
         $(this).remove();
+      }
     });
-
   });
-
 });
+
+/**
+ * Appends an item to a list from a textfield object.
+ * 
+ * The textfield object is cleared and refocused afterwards.
+ * @param {JQuery} $textfield JQuery reference to the textfield object to use.
+ */
+function appendItem($textfield) {
+  list.append("<li><input type=\"checkbox\">" + $textfield.val() + "</li>";);
+  $textfield.val("");
+  $textfield.focus();
+}
